1b604446ddb342b689d737022b36e700125674c6
Paula Miceli <mariapaulamiceli@gmail.com>
2017-12-24 16:57:37 -0300

    Validaciones login

--- Controladores/loginControlador.php
+++ Controladores/loginControlador.php
@@ -1,71 +1,74 @@
 <?php 
-	namespace Controladores; 
 
-	use DAOs\BDUsuario;
+namespace Controladores; 
 
-	class LoginControlador{
+use DAOs\BDUsuario;
 
-		public function index(){
-			$usuario = $this->getUsuarioLogueado();
-			if (is_null($usuario)) {
-				require_once "Vistas/Login.php";	
+class LoginControlador extends ControladorComun {
+
+	public function index(){
+		$usuario = $this->getUsuarioLogueado();
+		if (is_null($usuario)) {
+			require_once "Vistas/Login.php";	
+		} else {
+			if ($usuario->getAdmin() == 1) {
+				header("Location: ".HOST."/administrador/menu");
 			} else {
-				if ($usuario->getAdmin() == 1) {
-					header("Location: ".HOST."/administrador/menu");
-				} else {
-					header("Location: ".HOST."/cliente/menu");
-				}
+				header("Location: ".HOST."/cliente/menu");
 			}
 		}
+	}
 
-		public function procesarLogin($usuario, $contrasenia){
+	public function procesarLogin($usuario, $contrasenia){
+
+		try {
+			$usuario = trim($usuario);
+			if (strlen($usuario) == 0) {
+				throw new \Exception('El usuario esta vacio');
+			}
+
+			$contrasenia = trim($contrasenia);
+			if (strlen($contrasenia) == 0) {
+				throw new \Exception('La contrasena esta vacia');
+			}
+
 			$modelo = BDUsuario::getInstance();
 			$usuario = $modelo->getUsuarioPorUsername($usuario);
 			
-			try{
-				if (is_null($usuario)) {
-					throw new \Exception('El usuario no existe');
-				}else {
-					$this->setUsuarioEnSession($usuario);
-				}
-			} catch (\Exception $exception) {
-            	echo '<script> alert("'.$exception->getMessage().'"); </script>';
-            	require_once "Vistas/Login.php";
-        	}
-
-        	try{
-				if ($usuario->getContrasenia() !== $contrasenia) {
-					throw new \Exception('Contrasenia incorrecta');
-				}else {
-					if ($usuario->getAdmin() == 1) {
-						header("Location: ".HOST."/administrador/menu");
-						return;
-					} else {
-						header("Location: ".HOST."/cliente/menu");
-						return;
-					}
-				}
-			} catch (\Exception $exception) {
-            	echo '<script> alert("'.$exception->getMessage().'"); </script>';
-            	require_once "Vistas/Login.php";
-        	}			
-		}
-
-		protected function setUsuarioEnSession($usuario) {
-			$_SESSION['USUARIO-LOGUEADO'] = $usuario;
-		}
-
-		public static function getUsuarioLogueado() {
-			if (isset($_SESSION['USUARIO-LOGUEADO'])) {
-				$usuario = $_SESSION['USUARIO-LOGUEADO'];
-				return $usuario;
-			} else {
-				return null;
+			if (is_null($usuario)) {
+				throw new \Exception('El usuario no existe');
 			}
-		}
 
+			if ($usuario->getContrasenia() !== $contrasenia) {
+				throw new \Exception('Contrasenia incorrecta');
+			}
+
+			$this->setUsuarioEnSession($usuario);
+
+			if ($usuario->getAdmin() == 1) {
+				header("Location: ".HOST."/administrador/menu");
+				exit;
+			} else {
+				header("Location: ".HOST."/cliente/menu");
+				exit;
+			}
+
+		} catch (\Exception $exception) {
+        	echo '<script> alert("'.$exception->getMessage().'"); </script>';
+        	require_once "Vistas/Login.php";
+    	}			
 	}
 
+	protected function setUsuarioEnSession($usuario) {
+		$_SESSION['USUARIO-LOGUEADO'] = $usuario;
+	}
 
-
-?>
+	public static function getUsuarioLogueado() {
+		if (isset($_SESSION['USUARIO-LOGUEADO'])) {
+			$usuario = $_SESSION['USUARIO-LOGUEADO'];
+			return $usuario;
+		} else {
+			return null;
+		}
+	}
+}
