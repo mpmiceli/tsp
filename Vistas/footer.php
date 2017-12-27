3fd2d518315f5531edb1c701a5aaf7c4b842107a
ecs90 <ecs_salas@hotmail.com>
2017-12-27 19:54:48 -0300

    Eliminando integrity en los tags de css

--- Vistas/footer.php
+++ Vistas/footer.php
@@ -1,7 +1,7 @@
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="<?php echo HOST; ?>/Vistas/js/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-    <script src="<?php echo HOST; ?>/Vistas/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    <script src="<?php echo HOST; ?>/Vistas/js/popper.min.js" crossorigin="anonymous"></script>
+    <script src="<?php echo HOST; ?>/Vistas/js/bootstrap.min.js" crossorigin="anonymous"></script>
 </body>
 </html>
--- Vistas/header.php
+++ Vistas/header.php
@@ -10,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="<?php echo HOST; ?>/Vistas/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo HOST; ?>/Vistas/css/bootstrap.min.css" crossorigin="anonymous">
     <script src="<?php echo HOST; ?>/Vistas/js/jquery-3.2.1.min.js" crossorigin="anonymous"></script>
 	<link rel="stylesheet" type="text/css" href="<?php echo HOST; ?>/Vistas/css/style.css">
 
